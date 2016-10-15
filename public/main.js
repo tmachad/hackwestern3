87605bbdf40cc89a1a313beffe7dd6c30bd7908a
Tom Machado <tmachad3@uwo.ca>
2016-10-15 10:26:31 -0400

    Added client movement message loop and movement calc

--- public/main.js
+++ public/main.js
@@ -1,11 +1,30 @@
 $(function() {
 
 	var socket = io.connect();
+	var oldPos = {x:0, y:0};
+	var movement = {x:0.0, y:0.0};
+	var frameDelay = 1000/30;
+	
+	
+	window.addEventListener("mousemove", function (event) {
+		movement.x = event.pageX - $(window).width() / 2.0;
+		movement.y = event.pageY - $(window).height() / 2.0;
 
+		var length = Math.sqrt(Math.pow(movement.x, 2) + Math.pow(movement.y, 2));
+
+		//convert to unit vector
+		movement.x = movement.x / length;
+		movement.y = movement.y / length;
+		
+	});
+	
 	socket.on("new message", function(data) {
-		$("p").text(data);
+		//$("p").text(data);
 	});
 
-	alert("Hello World");
-
+	var intervalVar = setInterval(gameLoop, frameDelay);
+	
+	function gameLoop() {
+		socket.emit('player update', movement);
+	}
 });
